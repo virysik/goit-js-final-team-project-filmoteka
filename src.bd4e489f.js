3eb926553c6050b6128d55df15f5a144e7bdc4c4
virysik <68701496+virysik@users.noreply.github.com>
2021-06-07 11:15:51 +0000

    Deploying to gh-pages from @ virysik/goit-js-final-team-project-filmoteka@c006c9654257626dba8654ff3745459d1037d092 🚀

--- src.bd4e489f.js
+++ src.bd4e489f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-final-team-project-filmoteka/src.642437e7.js.map
+//# sourceMappingURL=/goit-js-final-team-project-filmoteka/src.bd4e489f.js.map
