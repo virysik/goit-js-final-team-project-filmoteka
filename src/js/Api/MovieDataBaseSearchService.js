fc923500847be33a0fee5298389cf5dfc576bd38
NikitaIskorka <67067414+NikitaIskorka@users.noreply.github.com>
2021-06-09 21:15:22 +0200

    Добавил свойствам пиватность

--- src/js/Api/MovieDataBaseSearchService.js
+++ src/js/Api/MovieDataBaseSearchService.js
@@ -2,14 +2,14 @@
  import axios from 'axios'
 class MovieDataBaseSearchService {
   constructor() {
-    this.searchQuery = '';
-    this.page = 1
+    this._searchQuery = '';
+    this._page = 1
     }
   async fetchMoviesByName() {
     
     const APIKEY = 'fce924273e7307535891dd09fc2f7662'
  
-      const url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${this.searchQuery}&page=${this.page}&include_adult=false`
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${this._searchQuery}&page=${this._page}&include_adult=false`
 
     const response = await axios(url);
     
@@ -19,17 +19,17 @@ class MovieDataBaseSearchService {
     //     return ''
     // }
     // Переходит на след страницу поиска
-    this.page += 1;
+    this._page += 1;
     return response.results;
     }
   get query() {
-  return this.searchQuery
+  return this._searchQuery
   }
   set query(newQuery) {
-    this.searchQuery=newQuery
+    this._searchQuery=newQuery
   }
    resetPage() {
-    this.page = 1
+    this._page = 1
   }
 
 }
