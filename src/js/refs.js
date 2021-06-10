216f5db4a00f08a66093583dc987f971b06eba11
Veronika Sirak <7nicka@gmail.com>
2021-06-10 13:17:55 +0100

    add function ligthbox

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,7 @@ const refs = {
   teamList: document.querySelector('.team-list'),
   teamBtn: document.querySelector('.page-footer__action-btn'),
   lightboxBtn: document.querySelector('.lightbox__button'),
+  lightboxContainer: document.querySelector('.js-lightbox'),
 };
 
 export default refs;
