75867ad4578d3b1a5426f051332d96376d0d7901
Veronika Sirak <7nicka@gmail.com>
2021-06-10 20:28:30 +0100

    import js file

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import './js/Api/MovieDataBaseSearchService';
 import './js/Api/genreTranspil';
 import getMovies from './js/Api/genreTranspil';
 import clearContainer from './js/search-on-query';
-
+import './js/team.js';
 getMovies();
 
 document.querySelector('[href="#home"]').addEventListener('click', getMovies);
 document.querySelector('[href="#library"]').addEventListener('click', clearContainer);
-
-import './js/team.js';
