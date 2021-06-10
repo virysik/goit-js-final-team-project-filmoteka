c1577ac9157dc3bc5aead06c0c84f8e31cb5e3b4
NikitaIskorka <67067414+NikitaIskorka@users.noreply.github.com>
2021-06-10 12:19:49 +0200

    Рендер правильных годов

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './sass/main.scss';
 //import * as basicLightbox from 'basiclightbox';
 //import '/basiclightbox/dist/basicLightbox.min.css';
 import './js/Api/MovieDataBaseSearchService'
+import './js/render-year-on-movie-card'
