62c85ed6c91e3def9f1701ed13b90380612533b2
Veronika Sirak <7nicka@gmail.com>
2021-06-10 12:15:08 +0100

    add lightbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 import './js/toggleHeaders';
 //import * as basicLightbox from 'basiclightbox';
-//import '/basiclightbox/dist/basicLightbox.min.css';
+// import '/basiclightbox/dist/basicLightbox.min.css';
 import './js/Api/MovieDataBaseSearchService';
+
 import './js/team.js';
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,1 +1,7 @@
+const refs = {
+  teamList: document.querySelector('.team-list'),
+  teamBtn: document.querySelector('.page-footer__action-btn'),
+  lightboxBtn: document.querySelector('.lightbox__button'),
+};
 
+export default refs;
