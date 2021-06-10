111667b47f934e7b13945587b664bfdbcef2f4a8
Veronika Sirak <7nicka@gmail.com>
2021-06-10 12:15:08 +0100

    add lightbox

--- src/js/team.js
+++ src/js/team.js
@@ -1,10 +1,25 @@
 import teamOfList from './teamList.js';
 import teamCardTpl from '../templates/team-card.hbs';
+import refs from './refs.js';
+import * as basicLightbox from 'basiclightbox';
+// import '/basiclightbox/dist/basicLightbox.min.css';
 
-const teamList = document.querySelector('.team-list');
+refs.teamBtn.addEventListener('click', onTeamBtnClick);
+refs.lightboxBtn.addEventListener('click', onLightboxBtn);
 
+function onTeamBtnClick() {
+  const modal = basicLightbox.create(document.querySelector('.lightbox'));
+  modal.show();
+}
+
+function onLightboxBtn() {
+  const modal = basicLightbox.create(document.querySelector('.lightbox'));
+  modal.close();
+}
 markupTeamCards(teamOfList);
 
 function markupTeamCards(team) {
-  teamList.insertAdjacentHTML('beforeend', teamCardTpl(team));
+  refs.teamList.insertAdjacentHTML('beforeend', teamCardTpl(team));
 }
+
+// console.log(basicLightbox);
