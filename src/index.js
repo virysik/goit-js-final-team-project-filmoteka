61857599287df5b178312a0a1fc96a079b6fbee6
Veronika Sirak <7nicka@gmail.com>
2021-06-10 03:06:56 +0100

    add team-modal

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './sass/main.scss';
 import './js/toggleHeaders';
 //import * as basicLightbox from 'basiclightbox';
 //import '/basiclightbox/dist/basicLightbox.min.css';
-import './js/Api/MovieDataBaseSearchService'
+import './js/Api/MovieDataBaseSearchService';
+import './js/team.js';
---
+++ src/js/teamList.js
@@ -0,0 +1,65 @@
+import photoNika from '../img/team/nika.jpg';
+import photoCat from '../img/team/cat.jpg';
+
+export default [
+  {
+    name: 'Vera',
+    photo: photoCat,
+    position: 'Team Leader',
+    git: 'https://github.com/virysik',
+  },
+  {
+    name: 'Veronika',
+    photo: photoNika,
+    position: 'Scrum Master',
+    git: 'https://github.com/nika-sirak',
+  },
+  {
+    name: 'Nikita',
+    photo: photoCat,
+    position: 'Developer',
+    git: 'https://github.com/NikitaIskorka',
+  },
+  {
+    name: 'Dmytro',
+    photo: photoCat,
+    position: 'Developer',
+    git: 'https://github.com/DmytroM029',
+  },
+  {
+    name: 'Denis',
+    photo: photoCat,
+    position: 'Developer',
+    git: 'https://github.com/Fadeev2000',
+  },
+  {
+    name: 'Galina',
+    photo: photoCat,
+    position: 'Developer',
+    git: 'https://github.com/GalinaP99',
+  },
+  {
+    name: 'Ivanka',
+    photo: photoCat,
+    position: 'Developer',
+    git: 'https://github.com/virysik',
+  },
+  {
+    name: 'Mikhail',
+    photo: photoCat,
+    position: 'Developer',
+    git: 'https://github.com/maralov',
+  },
+  {
+    name: 'Viktor',
+    photo: photoCat,
+    position: 'Developer',
+    git: 'https://github.com/KViktorK',
+  },
+  {
+    name: 'Vlad',
+    photo: photoCat,
+    position: 'Developer',
+    git: 'https://github.com/VladHlushets',
+  },
+];
