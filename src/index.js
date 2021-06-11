1d69b3a8b92c8ee0a46b0035fb19321356f04c83
virysik <68701496+virysik@users.noreply.github.com>
2021-06-11 16:24:31 +0200

    added trial test markUp in index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import './js/toggleHeaders';
 //import * as basicLightbox from 'basiclightbox';
 //import '/basiclightbox/dist/basicLightbox.min.css';
 import './js/Api/MovieDataBaseSearchService';
-import './js/Api/genreTranspil';
-import getMovies from './js/Api/genreTranspil';
+import ApiServiceMarkup from './js/Api/ApiServiceMarkUp';
 import clearContainer from './js/search-on-query';
 
-getMovies();
+// document.querySelectorAll('[href="#home"]').forEach(e => e.addEventListener('click', getMovies));
+// document.querySelector('[href="#library"]').addEventListener('click', clearContainer);
 
-document.querySelector('[href="#home"]').addEventListener('click', getMovies);
-document.querySelector('[href="#library"]').addEventListener('click', clearContainer);
+const apiData = new ApiServiceMarkup();
+apiData.getMarkUp();
