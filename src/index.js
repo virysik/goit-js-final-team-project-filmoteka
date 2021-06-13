353a31191a17eeb28c6c3a36c3f163a4625d91a5
virysik <68701496+virysik@users.noreply.github.com>
2021-06-13 21:53:46 +0200

    deleted repeating files

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import ApiServiceMarkup from './js/Api/api-service-markup';
 
 import './sass/main.scss';
 import './js/scroll-to-top';
-import './js/Api/movie-data-base-search-service';
 import './js/Api/auth';
 import './js/toggle-headers';
 import './js/theme-switch';
 import './js/modal-film-card';
 
-import './js/Api/movie-data-base-search-service';
 import './js/team.js';
-
 import './js/Api/auth';
 import * as modulPagination from './js/pagination';
 
