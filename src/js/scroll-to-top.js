ed0420d37cfbc1463a96e0d720e7c1600cef9590
Veronika Sirak <7nicka@gmail.com>
2021-06-18 13:14:14 +0100

    fix bug

--- src/js/scroll-to-top.js
+++ src/js/scroll-to-top.js
@@ -1,39 +1,31 @@
+const scrollToTopBtn = document.querySelector('.scrollToTopBtn');
+const paginationRef = document.querySelector('.pagination__list');
 
-const scrollToTopBtn = document.querySelector(".scrollToTopBtn"); 
-const paginationRef = document.querySelector('.pagination__list')
-
-
-
-scrollToTopBtn.addEventListener("click", scrollToTop);
+scrollToTopBtn.addEventListener('click', scrollToTop);
 
 function scrollToTop() {
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: 'smooth',
   });
 }
 
 const options = {
-    rootMargin: '40%',
-    threshold: 0.5
-}
+  rootMargin: '40%',
+  threshold: 0.5,
+};
 
+let callback = function (entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      scrollToTopBtn.classList.remove('is-hidden');
+    } else {
+      scrollToTopBtn.classList.add('is-hidden');
+      document.querySelector('.main__library-login').classList.add('is-hidden');
+    }
+  });
+};
 
-let callback = function(entries){
-    entries.forEach(entry => {
+const observer = new IntersectionObserver(callback, options);
 
- if(entry.isIntersecting)
-       {
-     scrollToTopBtn.classList.remove('is-hidden')
-
- }
- else {
-      scrollToTopBtn.classList.add('is-hidden')
-  }      
-    })
-}
-
-
-const observer = new IntersectionObserver(callback, options)
-
-observer.observe(paginationRef)
+observer.observe(paginationRef);
